377d5cca9893f3ea78e663bba64e615e756b7bca
Darius Cepulis <dcepulis@mux.com>
2023-07-27 14:09:21 -0500

    Cache asset.get

--- app/video/[assetId]/page.tsx
+++ app/video/[assetId]/page.tsx
@@ -1,6 +1,11 @@
 import MuxPlayerReact from "./player";
 import Video from "../../mux";
 import { notFound } from "next/navigation";
+import { cache } from "react";
+
+const getAssetForId = cache(async function (assetId: string) {
+  return await Video.Assets.get(assetId);
+});
 
 type VideoPageProps = {
   params: {
@@ -11,7 +16,7 @@ export default async function VideoPage({ params }: VideoPageProps) {
   const { assetId } = params;
   if (!assetId) notFound();
 
-  const asset = await Video.Assets.get(assetId);
+  const asset = await getAssetForId(assetId);
   const playbackId = asset?.playback_ids?.[0]?.id;
   if (!playbackId) notFound();
 
